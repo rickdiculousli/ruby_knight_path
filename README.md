eba40e4b03c9e62cb9d839e17739936a3a82a68d
Rick Li <li.rick.1996@gmail.com>
2018-08-15 14:27:58 +0800

    BFS implemented.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Knight Path
+
+A breadth first search algorithm for shortest path a knight can take to get to another point on the chessboard
+
+## How to run
+
+Change directory into project, then run Ruby intepretor on the Game.rb file
+
+```
+$ cd ./ruby_knight_path
+$ ruby knightpath.rb
+```
+
+Input Ctrl+C at any time to exit
+
+
+## Built With
+
+* [Ruby](https://www.ruby-lang.org/) - Version 2.4.1
+
+
+## Author
+
+* **Rick Li** - [Linkedin](https://www.linkedin.com/in/rickdiculousli/)
+
+
+## Acknowledgments
+
+* [The Odin Project](https://www.theodinproject.com) for creating and maintaining the curriculum for aspiring ruby developers.
+
--- knightpath.rb
+++ knightpath.rb
@@ -31,12 +31,10 @@ def create_board
       nodes[[x,y]] = Node.new([x,y])
     end
   end
-  Kernel.puts "  created nodes!"
   nodes.each do |key,val|
     paths(key).each do |o_key|
       val.children << nodes[o_key] unless nodes[o_key].nil?
     end
-    Kernel.puts "  gave #{val.val}: #{val.children.map{|x| x.val}} children"
   end
   nodes
 end
@@ -44,26 +42,52 @@ end
 def knight_path(start,goal,nodes)
   root = nodes[start]
   dest = nodes[goal]
+  Kernel.puts "  start at: #{root.val}"
+  Kernel.puts "  end at: #{dest.val}"
   known_nodes = []
   queue = [root]
 
   until queue.empty?
     test = queue.shift
-    puts "#{queue.size}"
     if test == dest
+      test.parents << test
       return test.parents
     else
       avail_paths = test.children - known_nodes
       avail_paths.each do |n|
+        n.parents.concat(test.parents)
         n.parents << test
       end
       queue.concat(avail_paths)
       known_nodes.concat(avail_paths)
     end
   end
+  nil
 end
 puts "begin app!"
 board = create_board
 puts "created board!"
-# pknight_path([0,0],[4,4], board)
+puts "begin pathfinding!"
+path = knight_path([0,0],[5,6], board)
+puts "path found!"
+puts "Path route: #{path.map{|x| x.val}}"
+sleep 2
+puts "Now test your own!"
+loop do
+  puts "input starting coordinates: '0-7,0-7'"
+  s = gets.chomp.split(",")
+  s = [s[0].to_i,s[1].to_i]
+  puts "input destination coordinates: '0-7,0-7'"
+  d = gets.chomp.split(",")
+  d = [d[0].to_i,d[1].to_i]
+  b = create_board
+  path = knight_path(s,d, b)
+  puts "Path route: #{path.map{|x| x.val}}"
+  4.times do
+    sleep 1
+    print "."
+  end
+  system "clr" or system "clear"
+end
+
 
